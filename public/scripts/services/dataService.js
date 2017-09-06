7ad33eb534ea331a5b3859cdb68b43293b8e9867
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-06 22:47:31 +0200

    Update the data service

--- public/scripts/services/dataService.js
+++ public/scripts/services/dataService.js
@@ -1,11 +1,19 @@
 (function () {
   'use strict';
 
+
+  /*
+    This service makes all calls to the REST API
+    --
+    Few things to have in mind:
+      - see if the request if parameters are made properly
+      - see how post, put and delete methods work by looking for snippets
+  */
   angular
     .module('app')
     .service('dataService', function ($http) {
 
-      // Get all recipes from the express rest api
+      // Gets all recipes
       this.getAllRecipes = function (callbackSuccess, callbackError) {
         $http
           .get('/api/recipes')
@@ -13,5 +21,68 @@
           .catch(callbackError)
       };
 
+
+			// Gets all categories
+      this.getAllCategories = function (callbackSuccess, callbackError) {
+        $http
+          .get('/api/categories')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
+
+
+      // Gets all food items
+      this.getAllFoodItems = function (callbackSuccess, callbackError) {
+        $http
+          .get('/api/fooditems')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
+
+
+      // Gets all of the recipes for the specified category
+      this.getAllRecipesByCategory = function (callbackSuccess, callbackError) {
+        $http
+          // Maybe have to change the line below
+          .get('/api/recipes?category={category}')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
+
+
+      // Gets the recipe for the specified ID
+      this.getRecipeById = function (callbackSuccess, callbackError) {
+        $http
+          .get('/api/recipes/{id}')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
+
+
+      // Updates the recipe for the specified ID
+      this.updateRecipeById = function (callbackSuccess, callbackError) {
+        $http
+          .put('/api/recipes/{id}')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
+
+
+      // Adds a recipe
+      this.addRecipe = function (callbackSuccess, callbackError) {
+        $http
+          .post('/api/recipes')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
+
+
+      // Deletes the recipe for the specified ID
+      this.deleteRecipe = function (callbackSuccess, callbackError) {
+        $http
+          .delete('/api/recipes/{id}')
+          .then(callbackSuccess)
+          .catch(callbackError)
+      };
     });
 })();
