3799f4d279be910ae980dc532022cb41bb35097f
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-08 12:06:02 +0200

    Show all recipes within the recipes template

--- public/scripts/controllers/RecipesController.js
+++ public/scripts/controllers/RecipesController.js
@@ -5,6 +5,18 @@
     .module('app')
     .controller('RecipesController', function ($scope, dataService) {
 
+      // Get all recipes from the database in order to display them
+      dataService.getAllRecipes(
+        // If correct
+        function (response) {
+          $scope.recipes = response.data;
+        },
+        // If not, for now, show error within the console
+        function (error) {
+          console.log(error);
+        }
+      );
+
       // When the user wants to add a new recipe
       $scope.addRecipe = function () {
         console.log('The user wants to create a new recipe');
