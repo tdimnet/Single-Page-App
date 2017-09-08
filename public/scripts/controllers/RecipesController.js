759c76ec1e855d04f5a5102825568670d9eab377
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-08 12:13:37 +0200

    Start firing actions when the user clicks on particular links

--- public/scripts/controllers/RecipesController.js
+++ public/scripts/controllers/RecipesController.js
@@ -17,9 +17,25 @@
         }
       );
 
+
       // When the user wants to add a new recipe
       $scope.addRecipe = function () {
         console.log('The user wants to create a new recipe');
+      };
+
+      // See recipe in detail
+      $scope.seeRecipe = function () {
+        console.log('The user wants to see the desired recipe');
+      };
+
+      // edit the desired recipe
+      $scope.editRecipe = function () {
+        console.log('The user wants to edit the desired recipe');
+      };
+
+      // Delete the desired recipe
+      $scope.deleteRecipe = function () {
+        console.log('The user wants to delete the desired recipe');
       };
 
     });
