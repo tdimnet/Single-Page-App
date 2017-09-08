3486729cd56c2160581c4b8b997389a9a6d788ba
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-08 11:58:31 +0200

    Fire a new action when the user clicks the add recipe button

--- public/scripts/controllers/RecipesController.js
+++ public/scripts/controllers/RecipesController.js
@@ -5,5 +5,10 @@
     .module('app')
     .controller('RecipesController', function ($scope, dataService) {
 
+      // When the user wants to add a new recipe
+      $scope.addRecipe = function () {
+        console.log('The user wants to create a new recipe');
+      };
+
     });
 })();
