8e2e305a744e327dbe583044e22a6b111b000045
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-13 18:55:33 +0200

    Add security when no recipes are found and a way to the /add url

--- public/scripts/controllers/RecipeDetailController.js
+++ public/scripts/controllers/RecipeDetailController.js
@@ -4,8 +4,6 @@
   angular
     .module('app')
     .controller('RecipeDetailController', function ($scope, $location, $routeParams, dataService) {
-      console.log($routeParams.id);
-
 			//
 				// Controller functions
 			//
@@ -21,20 +19,20 @@
 			//
 				// The database calls
 			//
-      // Get the recipe according to the current routeParams ID
-      dataService.getRecipeById(
-        // The recipe ID
-        $routeParams.id,
-        // If correct
-        function (response) {
-					$scope.recipe = response.data;
-        },
-        // If not
-        function (error) {
-          $location.path('/');
-        }
-      );
-
-
+      // Get the recipe according to the current routeParams ID when the path is /edit
+			if ($location.url() !== '/add') {
+				dataService.getRecipeById(
+					// The recipe ID
+					$routeParams.id,
+					// If correct
+					function (response) {
+						$scope.recipe = response.data;
+					},
+					// If not, redirect to the home page
+					function (error) {
+						$location.path('/');
+					}
+				);
+			}
     });
 })();
--- public/scripts/controllers/RecipesController.js
+++ public/scripts/controllers/RecipesController.js
@@ -3,8 +3,20 @@
 
   angular
     .module('app')
-    .controller('RecipesController', function ($scope, dataService) {
-
+    .controller('RecipesController', function ($scope, $location, dataService) {
+			
+			//
+				// Controller functions
+			//
+			$scope.addRecipe = function() {
+				$location.path('/add');
+			};
+			
+			
+			
+			//
+				// The database calls
+			//
       // Get all recipes from the database in order to display them
       dataService.getAllRecipes(
         // If correct
@@ -28,27 +40,5 @@
           console.log(error);
         }
       );
-
-
-      // When the user wants to add a new recipe
-      $scope.addRecipe = function () {
-        console.log('The user wants to create a new recipe');
-      };
-
-      // See recipe in detail
-      $scope.seeRecipe = function () {
-        console.log('The user wants to see the desired recipe');
-      };
-
-      // edit the desired recipe
-      $scope.editRecipe = function () {
-        console.log('The user wants to edit the desired recipe');
-      };
-
-      // Delete the desired recipe
-      $scope.deleteRecipe = function () {
-        console.log('The user wants to delete the desired recipe');
-      };
-
     });
 })();
