776125e703b4ba751178c0f04379de9e7c8d4e56
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-13 14:25:57 +0200

    Add a way to move back and forth for the recipe

--- public/scripts/controllers/RecipeDeleteController.js
+++ public/scripts/controllers/RecipeDeleteController.js
@@ -4,8 +4,37 @@
   angular
     .module('app')
     .controller('RecipeDeleteController', function ($scope, $location, $routeParams, dataService) {
-
-      console.log($routeParams.id);
+			
+			//
+				// Controller functions
+			//
+			// Deleting recipe when the delete button is clicked
+			$scope.confirmDetele = function() {
+				console.log('the user wants to delete the recipe');
+			};
+			
+			// Return to the home page of the web site
+			$scope.cancelDetele = function() {
+				$location.path('/');
+			};
+			
+			
+			//
+				// The database calls
+			//
+      // Get the recipe according to the route parameters
+			dataService.getRecipeById(
+				// the ID of the recipe comming from the route parameter
+				$routeParams.id,
+				// If the id exists
+				function(response) {
+					$scope.recipe = response.data;
+				},
+				// If not, redirect to the home page
+				function(error) {
+					console.log(error);
+				}
+			);
 
     });
 })();
--- public/scripts/controllers/RecipeDetailController.js
+++ public/scripts/controllers/RecipeDetailController.js
@@ -6,6 +6,21 @@
     .controller('RecipeDetailController', function ($scope, $location, $routeParams, dataService) {
       console.log($routeParams.id);
 
+			//
+				// Controller functions
+			//
+			// Main functions ?
+			
+			// Return to the home page of the web site
+			$scope.cancelEdit = function() {
+				$location.path('/');
+			};
+			
+			
+			
+			//
+				// The database calls
+			//
       // Get the recipe according to the current routeParams ID
       dataService.getRecipeById(
         // The recipe ID
