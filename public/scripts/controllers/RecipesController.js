f8bd0d4db956497e5ec780057c23a32ab0183912
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-09-16 07:23:42 +0200

    Display the recipes according to the choosen caterogy and when the error message when no recipes have been found

--- public/scripts/controllers/RecipesController.js
+++ public/scripts/controllers/RecipesController.js
@@ -13,21 +13,30 @@
 			//
 				// Controller functions
 			//
+			// Go to the add Recipe when add path is trigered
 			$scope.addRecipe = function() {
 				$location.path('/add');
 			};
 			
+			// Filter the recipe according to the caterogy choosen
 			$scope.filterRecipeByCategory = function() {
 				var category = $scope.currentCategory ? $scope.currentCategory.name : "";
 				dataService.getRecipesByCategory(
 					category,
 					function (response) {
-						console.log(response);
+						$scope.recipes = response.data;
 					},
 					function (error) {
 						console.log(error);
 					}
 				);
+			};
+			
+			// If there are not recipes to display, show the message
+			$scope.noRecipeFromFilter = function() {
+				if ($scope.recipes !== undefined) {
+					return $scope.recipes.length === 0;
+				}
 			};
 			
 			
